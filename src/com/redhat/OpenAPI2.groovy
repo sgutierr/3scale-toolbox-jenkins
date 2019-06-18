cd1851a8b4b2c403351cb660b1e9e190fe8b8324
Nicolas Massé <nicolas.masse@itix.fr>
2019-06-18 15:11:02 +0200

    Avoid Jenkins sandbox restriction on java.util.LinkedHashMap

--- src/com/redhat/OpenAPI2.groovy
+++ src/com/redhat/OpenAPI2.groovy
@@ -5,7 +5,11 @@ package com.redhat
 enum ThreescaleSecurityScheme {
   OPEN,
   APIKEY,
-  OIDC
+  OIDC;
+
+  // Avoid Jenkins sandbox restriction on java.util.LinkedHashMap
+  // by providing an empty constructor
+  public ThreescaleSecurityScheme() {}
 }
 
 class OpenAPI2 {
