afaba5de6df4be6532a62c3273a5045a02affb4c
Sergio <sgutierr@redhat.com>
2020-10-28 10:26:21 +0100

    Update ThreescaleService.groovy

--- src/com/redhat/ThreescaleService.groovy
+++ src/com/redhat/ThreescaleService.groovy
@@ -3,71 +3,137 @@
 package com.redhat
 
 class ThreescaleService {
-  OpenAPI2 openapi
-  List<ApplicationPlan> applicationPlans
-  ToolboxConfiguration toolbox
-  ThreescaleEnvironment environment
+    OpenAPI openapi
+    List<ApplicationPlan> applicationPlans
+    List<Application> applications
+    ToolboxConfiguration toolbox
+    ThreescaleEnvironment environment
+    Util util
 
-  void importOpenAPI() {
-    Util util = new Util()
+    void importOpenAPI() {
+        util = new Util()
 
-    if (this.environment.stagingPublicBaseURL != null || this.environment.productionPublicBaseURL != null) {
-      // See https://issues.jboss.org/browse/THREESCALE-2607
-      throw new Exception("NOT_IMPLEMENTED")
+        def baseName = util.basename(this.openapi.filename)
+        def globalOptions = toolbox.getGlobalToolboxOptions()
+        def commandLine = ["3scale", "import", "openapi"] + globalOptions + ["-t", this.environment.targetSystemName, "-d", this.toolbox.destination, "/artifacts/${baseName}"]
+        if (this.environment.stagingPublicBaseURL != null) {
+            commandLine += "--staging-public-base-url=${this.environment.stagingPublicBaseURL}"
+        }
+        if (this.environment.productionPublicBaseURL != null) {
+            commandLine += "--production-public-base-url=${this.environment.productionPublicBaseURL}"
+        }
+        if (this.environment.privateBaseUrl != null) {
+            commandLine += "--override-private-base-url=${this.environment.privateBaseUrl}"
+        }
+
+        if (this.openapi.securityScheme == ThreescaleSecurityScheme.OPEN) {
+            commandLine += "--default-credentials-userkey=${this.applications[0].userkey}"
+        }
+
+        if (this.openapi.securityScheme == ThreescaleSecurityScheme.OIDC) {
+            commandLine += "--oidc-issuer-endpoint=${this.environment.oidcIssuerEndpoint}"
+        }
+
+        if (this.environment.privateBasePath != null) {
+            commandLine += "--override-private-basepath=${this.environment.privateBasePath}"
+        }
+
+        if (this.environment.publicBasePath != null) {
+            commandLine += "--override-public-basepath=${this.environment.publicBasePath}"
+        }
+
+        if (!this.openapi.validateOAS) {
+            commandLine += "--skip-openapi-validation"
+        }
+
+        toolbox.runToolbox(commandLine: commandLine,
+                jobName: "import-openapi",
+                openAPI: [
+                        "filename": baseName,
+                        "content" : this.openapi.getUpdatedContent()
+                ])
     }
 
-    if (this.environment.privateBaseUrl != null) {
-      // See https://issues.jboss.org/browse/THREESCALE-2734
-      throw new Exception("NOT_IMPLEMENTED")
+    void applyApplicationPlans() {
+        def globalOptions = toolbox.getGlobalToolboxOptions()
+
+        this.applicationPlans.each {
+            def commandLine
+
+            if (it.artefactFile?.trim()) {
+                commandLine = ["3scale", "application-plan", "import", "-f"] + it.artefactFile + globalOptions + [this.toolbox.destination, this.environment.targetSystemName]
+            } else {
+                commandLine = ["3scale", "application-plan", "apply"] + globalOptions + [this.toolbox.destination, this.environment.targetSystemName, it.systemName]
+                commandLine += ["--approval-required=${it.approvalRequired}",
+                                "--cost-per-month=${it.costPerMonth}",
+                                "--name=${it.name}",
+                                "--publish=${it.published}",
+                                "--setup-fee=${it.setupFee}",
+                                "--trial-period-days=${it.trialPeriodDays}"]
+
+                if (it.defaultPlan) {
+                    commandLine += "--default"
+                }
+
+            }
+            toolbox.runToolbox(commandLine: commandLine,
+                    jobName: "apply-application-plan-${it.systemName}")
+        }
     }
 
-    // Compute the target system_name
-    this.environment.targetSystemName = (this.environment.environmentName != null ? "${this.environment.environmentName}_" : "") + this.environment.baseSystemName + "_${this.openapi.majorVersion}"
+    void applyApplication() {
 
-    def baseName = basename(this.openapi.filename)
-    def globalOptions = toolbox.getGlobalToolboxOptions()
-    def commandLine = "3scale import openapi ${globalOptions} -t ${this.environment.targetSystemName} -d ${this.toolbox.destination} /artifacts/${baseName}"
-    toolbox.runToolbox(commandLine: commandLine,
-                       jobName: "import",
-                       openAPI: [
-                         "filename": baseName,
-                         "content": util.readFile(this.openapi.filename)
-                       ])
-  }
+        def globalOptions = toolbox.getGlobalToolboxOptions()
+        this.applications.each {
+            def commandLine = ["3scale", "application", "apply"] + globalOptions + this.toolbox.destination
+            if (this.openapi.securityScheme == ThreescaleSecurityScheme.APIKEY
+                    || this.openapi.securityScheme == ThreescaleSecurityScheme.OPEN) {
 
-  void applyApplicationPlans() {
-    def globalOptions = toolbox.getGlobalToolboxOptions()
-    this.applicationPlans.each{
-      def commandLine = "3scale application-plan apply ${globalOptions} ${this.toolbox.destination} ${this.environment.targetSystemName} ${it.systemName} --approval-required=${it.approvalRequired} --cost-per-month=${it.costPerMonth} --end-user-required=${it.endUserRequired} --name=${it.name} --publish=${it.published} --setup-fee=${it.setupFee} --trial-period-days=${it.trialPeriodDays}"
-      if (it.defaultPlan) {
-        commandLine += " --default"
-      }
+                commandLine += it.userkey
+            } else if (this.openapi.securityScheme == ThreescaleSecurityScheme.OIDC) {
+                commandLine += [it.clientId, "--application-key=${it.clientSecret}"]
+            } else {
+                throw new Exception("NOT_IMPLEMENTED")
+            }
 
-      toolbox.runToolbox(commandLine: commandLine,
-                         jobName: "apply-application-plan-${it.systemName}")
+            commandLine += ["--name=${it.name}",
+                            "--description=${it.description != null ? it.description : "Created by the 3scale_toolbox from a Jenkins pipeline."}",
+                            "--plan=${it.plan}",
+                            "--service=${this.environment.targetSystemName}",
+                            "--account=${it.account}"]
+
+            // Disabled for now because of https://issues.jboss.org/browse/THREESCALE-2844
+            /*
+            if (it.active) {
+                commandLine += "--resume"
+            } else {
+                commandLine += "--suspend"
+            }
+            */
+
+            toolbox.runToolbox(commandLine: commandLine,
+                    jobName: "apply-application-${it.name}")
+        }
     }
-  }
 
-  void applyApplication(Map application) {
-    // See https://issues.jboss.org/browse/THREESCALE-2425
-    throw new Exception("NOT_IMPLEMENTED")
-  }
+    Map readProxy(String environment) {
+        assert environment != null
 
-  Map readProxy() {
-    // See https://issues.jboss.org/browse/THREESCALE-2405
-    throw new Exception("NOT_IMPLEMENTED")
-  }
+        def globalOptions = toolbox.getGlobalToolboxOptions()
+        def commandLine = ["3scale", "proxy-config", "show"] + globalOptions + [this.toolbox.destination, this.environment.targetSystemName, environment]
+        def proxyDefinition = toolbox.runToolbox(commandLine: commandLine,
+                jobName: "apply-proxy-config-show")
 
-  void promoteToProduction() {
-    // See https://issues.jboss.org/browse/THREESCALE-2405
-    throw new Exception("NOT_IMPLEMENTED")
-  }
+        return new Util().readJSON(proxyDefinition.stdout).content.proxy as Map
+    }
 
-  static String basename(path) {
-      def filename = path.drop(path.lastIndexOf("/") != -1 ? path.lastIndexOf("/") + 1 : 0)
-      filename = filename.replaceAll("[^-._a-zA-Z0-9]", "_")
-      return filename
-  }
+    void promoteToProduction() {
+
+
+        def globalOptions = toolbox.getGlobalToolboxOptions()
+        def commandLine = ["3scale", "proxy-config", "promote"] + globalOptions + [this.toolbox.destination, this.environment.targetSystemName]
+        toolbox.runToolbox(commandLine: commandLine,
+                jobName: "apply-proxy-config-promote")
+    }
 
 }
-
