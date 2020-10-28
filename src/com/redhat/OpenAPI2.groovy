caceabb87a4fceca4b2109daaab3ab95766bf35e
Sergio <sgutierr@redhat.com>
2020-10-28 10:27:22 +0100

    Update OpenAPI2.groovy

--- src/com/redhat/OpenAPI2.groovy
+++ src/com/redhat/OpenAPI2.groovy
@@ -2,22 +2,53 @@
 
 package com.redhat
 
-class OpenAPI2 {
-    String filename
-    def content
-    String version
-    String majorVersion
-
+class OpenAPI2 extends OpenAPI {
     OpenAPI2(Map conf) {
-        assert conf.filename != null
-        this.filename = conf.filename
+        super(conf);
     }
 
     void parseOpenAPISpecificationFile() {
-        this.content = new Util().readOpenAPISpecificationFile(this.filename)
         assert content.swagger == "2.0"
         this.version = content.info.version
         assert this.version != null
         this.majorVersion = version.tokenize(".")[0]
+
+        String securitySchemeName = null
+        if (content.security != null && content.security.size() == 1) {
+            Set securitySchemes = content.security[0].keySet()
+            if (securitySchemes.size() == 1) {
+                securitySchemeName = securitySchemes.first()
+            } else {
+                throw new Exception("Cannot handle OpenAPI Specifications with multiple security requirements or no global requirement. Found ${content.security.size()}/${securitySchemes.size()} security requirements.")
+            }
+
+        } else if ((content.security == null || content.security.size() == 0)
+                && (content.securityDefinitions == null || content.securityDefinitions.size() == 0)) {
+
+            // To make it explicit that this is an Open API, we require no security requirement
+            // and no security definition (better be safe than sorry...)
+            this.securityScheme = ThreescaleSecurityScheme.OPEN
+        } else {
+            throw new Exception("Cannot handle OpenAPI Specifications with multiple security requirements or no global requirement. Found ${content.security != null ? content.security.size() : "no"} security requirements.")
+        }
+
+        if (securitySchemeName != null) {
+            if (content.securityDefinitions != null
+             && content.securityDefinitions.get(securitySchemeName) != null) {
+
+                Map securityScheme = content.securityDefinitions.get(securitySchemeName)
+                String securityType = securityScheme.type
+
+                if (securityType == "oauth2") {
+                    this.securityScheme = ThreescaleSecurityScheme.OIDC
+                } else if (securityType == "apiKey") {
+                    this.securityScheme = ThreescaleSecurityScheme.APIKEY
+                } else {
+                    throw new Exception("Cannot handle OpenAPI Specifications with security scheme: ${securityType}")
+                }
+            } else {
+                throw new Exception("Cannot find security scheme ${securitySchemeName} in OpenAPI Specifications")
+            }
+        } // else: this is an Open API
     }
 }
