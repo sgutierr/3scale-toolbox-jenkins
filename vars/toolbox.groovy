e3ffd920d7efd8c9f38ca2a03ac52f6d17f7c46c
Sergio <sgutierr@redhat.com>
2020-10-28 10:57:41 +0100

    Update toolbox.groovy

--- vars/toolbox.groovy
+++ vars/toolbox.groovy
@@ -13,34 +13,79 @@ ThreescaleService prepareThreescaleService(Map conf) {
   assert conf.toolbox != null
   assert conf.service != null
   assert conf.applicationPlans != null
+  assert conf.applications != null
 
   List<ApplicationPlan> plans = []
   conf.applicationPlans.each{
-    if (it.systemName == null || it.name == null) {
-      throw new Exception("Missing property in application plan: name or systemName")
+    if ((it.systemName == null || it.name == null) && !it.artefactFile?.trim()) {
+      throw new Exception("Missing property in application plan: name or systemName or artefactFile ")
     }
 
     ApplicationPlan plan = new ApplicationPlan(it)
     plans.add(plan)
   }
 
-  OpenAPI2 openapi = new OpenAPI2(conf.openapi)
+  def content = new Util().readOpenAPISpecificationFile(conf.openapi.filename)
+  OpenAPI openapi = null
+  if (content.swagger != null && content.swagger == "2.0") {
+    openapi = new OpenAPI2(conf.openapi + [ content: content ])
+  } else if (content.openapi != null && content.openapi instanceof String && content.openapi.startsWith("3.0")) {
+    openapi = new OpenAPI3(conf.openapi + [ content: content ])
+  } else {
+      throw new Exception("Cannot determine the OpenAPI version from 'openapi: ${content.openapi}' and 'swagger: ${content.swagger}'!")
+  }
   openapi.parseOpenAPISpecificationFile()
+  openapi.updateTitleWithEnvironmentAndVersion(conf.environment.environmentName)
+
+  if (conf.environment.targetSystemName == null) {
+    // Compute the target system_name
+    conf.environment.targetSystemName = (conf.environment.environmentName != null ? "${conf.environment.environmentName}_" : "") + conf.environment.baseSystemName + "_${openapi.majorVersion}"
+  }
+
+  // compute the public base urls from system_name, version number and wildcard domain (for semantic versioning)
+  String apiBaseName = conf.environment.targetSystemName.replaceAll(/[^-0-9a-zA-Z]/, "-").toLowerCase()
+  if (conf.environment.stagingPublicBaseURL == null && conf.environment.publicStagingWildcardDomain != null) {
+    conf.environment.stagingPublicBaseURL = "https://${apiBaseName}.${conf.environment.publicStagingWildcardDomain}"
+  }
+  if (conf.environment.productionPublicBaseURL == null && conf.environment.publicProductionWildcardDomain != null) {
+    conf.environment.productionPublicBaseURL = "https://${apiBaseName}.${conf.environment.publicProductionWildcardDomain}"
+  }
+
   ThreescaleEnvironment environment = new ThreescaleEnvironment(conf.environment)
   ToolboxConfiguration toolbox = new ToolboxConfiguration(conf.toolbox + ["JOB_BASE_NAME": JOB_BASE_NAME, "BUILD_NUMBER": BUILD_NUMBER])
-  ThreescaleService service = new ThreescaleService([ "openapi": openapi, "environment": environment, "toolbox": toolbox, applicationPlans: plans ] + conf.service)
+
+  List<Application> apps = []
+  conf.applications.each{
+    if (it.account == null || it.name == null) {
+      throw new Exception("Missing property in application : name or account")
+    }
+
+    Map credentials = [:]
+    if ((openapi.securityScheme == ThreescaleSecurityScheme.OPEN
+      || openapi.securityScheme == ThreescaleSecurityScheme.APIKEY) && it.userkey == null) {
+
+        credentials = getDefaultApplicationCredentials(environment, toolbox, it.name).findAll { key, value -> key == "userkey" }
+    } else if (openapi.securityScheme == ThreescaleSecurityScheme.OIDC && it.clientId == null && it.clientSecret == null) {
+        credentials = getDefaultApplicationCredentials(environment, toolbox, it.name).findAll { key, value -> key == "clientId" || key == "clientSecret" }
+    }
+
+    Application app = new Application(it + credentials)
+    apps.add(app)
+  }
+
+  ThreescaleService service = new ThreescaleService([ "openapi": openapi, "environment": environment, "toolbox": toolbox, applicationPlans: plans ,"applications":apps] + conf.service)
 
   return service
 }
 
-Map getDefaultApplicationCredentials(ThreescaleService service, String applicationName) {
-  String targetSystemName = service.environment.targetSystemName
-  String destination = service.toolbox.destination
-  String secretName = service.toolbox.secretName
+Map getDefaultApplicationCredentials(ThreescaleEnvironment environment, ToolboxConfiguration toolbox, String applicationName) {
+  String targetSystemName = environment.targetSystemName
+  String destination = toolbox.destination
+  String secretName = toolbox.secretName
   String secret = ""
   if (secretName != null) {
     openshift.withCluster() {
-      openshift.withProject(service.toolbox.openshiftProject) {
+      openshift.withProject(toolbox.openshiftProject) {
         def secretObject = openshift.selector('secret', secretName).object()
         secret = secretObject.data[".3scalerc.yaml"]
       }
@@ -52,9 +97,8 @@ Map getDefaultApplicationCredentials(ThreescaleService service, String applicati
   def app_id = sha1 file: "app_id"
   def app_secret = sha1 file: "app_secret"
 
-  // TODO: return user_key OR client_id/client_secret depending on the security scheme of the OpenAPI
   return [
-    userKey: app_id,
+    userkey: app_id,
     clientId: app_id,
     clientSecret: app_secret
   ]
