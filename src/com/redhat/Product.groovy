d0b73b27bc1cc1e74028e05268ce17bbd7c3e29a
Sergio <sgutierr@redhat.com>
2021-06-17 09:41:36 +0200

    Update Product.groovy

--- src/com/redhat/Product.groovy
+++ src/com/redhat/Product.groovy
@@ -5,6 +5,11 @@ package com.redhat
 class Product {
 
     String productFile
+     
+    Product (Map conf) {
+        assert conf.productfile != null
+        this.productFile = conf.productfile
+    }
 
 }
 
