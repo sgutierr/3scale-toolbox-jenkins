3740ab50167c12f38af7e0c2adcccc3cf3c5fed3
Sergio <sgutierr@redhat.com>
2021-06-17 16:52:09 +0200

    Update ThreescaleService.groovy

--- src/com/redhat/ThreescaleService.groovy
+++ src/com/redhat/ThreescaleService.groovy
@@ -58,7 +58,7 @@ class ThreescaleService {
   void importProduct() {
         def globalOptions = toolbox.getGlobalToolboxOptions()
         def commandLine
-        commandLine = ["3scale", "product", "import", "-f"] + "https://raw.githubusercontent.com/sgutierr/APIProductCICDdemo/master/cicd/APIProduct.yaml" + globalOptions + this.environment.targetSystemName
+        commandLine = ["3scale", "product", "import", "-f"] + "https://raw.githubusercontent.com/sgutierr/APIProductCICDdemo/master/cicd/APIProduct.yaml" + globalOptions
         toolbox.runToolbox(commandLine: commandLine,
                     jobName: "import-3scale-product")
 
